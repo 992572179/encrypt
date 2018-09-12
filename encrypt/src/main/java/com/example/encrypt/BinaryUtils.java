1da3c814eadbf281c490a46e43ca8b78e5094246
kawano <1071405312@qq.com>
2018-09-12 14:32:26 +0800

    add functions for dec2binary

--- encrypt/src/main/java/com/example/encrypt/BinaryUtils.java
+++ encrypt/src/main/java/com/example/encrypt/BinaryUtils.java
@@ -1,9 +1,22 @@
 package com.example.encrypt;
 
+/**
+ * 
+ * @author kawano
+ * @date 2018年9月12日
+ * @description BinaryUtils
+ */
 public class BinaryUtils {
-	
+	private BinaryUtils() {
+		// private constructor.
+	}
+
 	public static String dec2binary(Integer i) {
 		return Integer.toBinaryString(i);
+	}
+
+	public static String dec2Hex(Integer i) {
+		return Integer.toHexString(i);
 	}
 
 }
